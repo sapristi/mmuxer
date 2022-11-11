a27334aaa025331ed7f73ee887551dd55e68af3d
Mathias Millet <mathias@mmill.eu>
2022-11-12 00:42:46 +0100

    add BatchMoveFilter

--- mail_juicer/models/filter.py
+++ mail_juicer/models/filter.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from imap_tools import BaseMailBox, MailMessage
 
 from .action import Action
@@ -5,8 +7,9 @@ from .common import BaseModel
 from .condition import Condition
 
 
-class Filter(BaseModel):
+class GenericFilter(BaseModel):
     name: str
+    type: Literal["generic"] = "generic"
     condition: Condition
     actions: list[Action]
     abort_next: bool = False
@@ -27,3 +30,26 @@ class Filter(BaseModel):
             applied = f.apply(mailbox, message)
             if applied and f.abort_next:
                 break
+
+
+class BatchMoveItem(BaseModel):
+    condition: Condition
+    dest: str
+    mark_read: bool = False
+
+    def apply(self, mailbox: BaseMailBox, message: MailMessage):
+        if self.condition.eval(message):
+            pass
+
+
+class BatchMoveFilter(BaseModel):
+    name: str
+    type: Literal["batch_move"] = "batch_move"
+    items: list[BatchMoveItem]
+
+    def apply(self, mailbox: BaseMailBox, message: MailMessage):
+        pass
+
+
+class Filter(BaseModel):
+    __root__: GenericFilter | BatchMoveFilter
