3ebbfec202d031f839478f65fd177c4939146e0f
Mathias Millet <mathias@mmill.eu>
2022-11-26 20:53:38 +0100

    test(parse_rule): test _actions

--- tests/models/test_parse_rule.py
+++ tests/models/test_parse_rule.py
@@ -7,4 +7,5 @@ def test_parse_rule(data):
     rules_yaml = data("models/rules.yaml")
     rules_dicts = yaml.safe_load(rules_yaml)
     for f in rules_dicts:
-        Rule.parse_obj(f)
+        r = Rule.parse_obj(f)
+        r._actions()
